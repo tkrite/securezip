6bafc03b885580078783ef9fd287359139e6dcc3
murakami-akane <murakami@tkrite.com>
2026-03-02 17:29:48 +0900

    feat(coredata): Core Data モデルの Attribute を定義

---
+++ SecureZip/SecureZip/SecureZip-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  SecureZip-Bridging-Header.h
+//  SecureZip
+//
+//  Created by Akane Murakami on 2026/03/02.
+//
+
+#ifndef SecureZip_Bridging_Header_h
+#define SecureZip_Bridging_Header_h
+
+
+#endif /* SecureZip_Bridging_Header_h */
